ec05555b22eed33693299e09811f80622795b24a
Mario Massari <mariomassari11@gmail.com>
2021-11-09 19:00:55 -0300

    resolucao exercicios variaveis javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,81 @@
+// modulo1 - Javascript - Exercicio variaveis
+
+// Exercícios de interpretação de código
+  // Exercicio 1
+   /* O primeiro console.log ira retornar o valor definido para b que é 10.
+    No segundo console.log ira retornar o valor definido de a que é 10, porem no b ele mostrara o valor de 5, que foi o ultimo valor definido para a variavel b.
+    No console, sera impresso o valor de 10 5 
+   */
+
+
+  // Exercicio 2
+   /* As variaveis a e b tiveram o valor definido no começo do codigo, porem ha uma reatribuicao no meio do caminho. Seguindo a logica de que o documento é lido de cima para baixo, ao executar as trocas matematicas todos os valores ficam iguais a 10. Logo sera impresso no console, 10 10 10.
+   */
+
+   // Exercicio 3
+    /*É um programa para calcular o valor recebido por hora. Divindo o total ganho por dia pelas horas trabalhadas.
+    para a variavel "p" poderiamos atribuir o nome horasTrabalhadasDia
+    para a variavel "t" poderiamos atribuir o nome valorRecebidoDia
+    */
+
+
+ 
+// Exercícios de escrita de código
+
+  // Exercicio 1
+
+  // let nome 
+  // let idade 
+  
+  // console.log(typeof nome, typeof idade)
+
+  // Sem os valores atribuidos, ao imprimir uma variavel no console ela retorna o tipo de variavel undefined
+
+  // nome = prompt("Qual é o seu nome?")
+  // idade = prompt("Qual é sua idade?")
+
+  // console.log(typeof nome, typeof idade)
+
+  // Com os novas atribuições, ambas as variaveis vao ter o tipo de String, por que tudo que retorna do prompt é lido como String, a não ser que haja uma conversao de tipos de variaveis. 
+
+  // console.log("Ola", nome + ",", "voce tem", idade, "anos.")
+
+  // Exercicio 2
+
+    // let ehBrasileiro = "Voce é brasileiro?"
+    // let gostaAzul = "Voce gosta da cor azul?"
+    // let teveTekpix = "Voce ja teve uma Tekpix?"
+
+    // let respostaEhBrasileiro = "Sim"
+    // let respostaGostaAzul = "Não"
+    // let respostaTeveTekpix = "Não"
+
+    // console.log(ehBrasileiro, "-", respostaEhBrasileiro, gostaAzul, respostaGostaAzul, "-", teveTekpix, respostaTeveTekpix)
+
+
+  // Exercicio 3
+
+    // let a = 10
+    // let b = 25
+    
+    // Aqui faremos uma lógica para trocar os valores
+    // let c 
+    // c = a
+    // a = b
+    // b = c
+
+    // Depois de trocados, teremos o seguinte resultado:
+    // console.log("O novo valor de a é", a) // O novo valor de a é 25
+    // console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
+    // * Desafio
+
+    let firstNumber = Number(prompt("Informe um numero: "))
+    let secondNumber = Number(prompt("Informe outro numero: "))
+
+    let x = firstNumber + secondNumber
+    let y = firstNumber * secondNumber
+
+    // console.log("O primeiro número somado ao segundo número resulta em: ", x, "O primeiro número multiplicado pelo segundo número resulta em: ", y)
+
