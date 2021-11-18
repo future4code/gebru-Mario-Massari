ad7b8c5c4e3326fe6643dc9fb95b6dcf28a49959
Mario Massari <mariomassari11@gmail.com>
2021-11-17 23:43:55 -0300

    exercicios strings e arrays resolvidos

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,78 @@
+// Strings e Arrays
+
+// Exercícios de interpretação de código
+// Exercicio 1
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+// Exercicio 2
+// Sera impresso: SUBI NUM ÔNIBUS EM MIRROCOS 27, onde 27 é a lenght da frase.
+
+// Exercícios de escrita de código
+// Exercicio 1
+// let emailDoUsuario = prompt("Qual é seu email?");
+// let nomeDoUsuario = prompt("Qual seu nome de usuario?");
+// console.log(
+//   `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`
+// );
+
+// Exercicio 2
+// let comidasPreferidas = ["Churrasco", "Pizza", "Sushi", "Macarrao", "Lanche"];
+// a)
+// console.log(comidasPreferidas);
+// b)
+// console.log(
+//   "Essas são as minhas comidas preferidas: ",
+//   "\n ",
+//   comidasPreferidas[0],
+//   "\n ",
+//   comidasPreferidas[1],
+//   "\n ",
+//   comidasPreferidas[2],
+//   "\n ",
+//   comidasPreferidas[3],
+//   "\n ",
+//   comidasPreferidas[4]
+// );
+// c)
+// let comidaUsuario = prompt("Informe sua comida preferida: ");
+// comidasPreferidas[1] = comidaUsuario;
+// console.log(comidasPreferidas);
+
+// Exercicio 3
+// a)
+// const listaDeTarefas = [];
+// b)
+// let tarefa1 = prompt("Informe a 1º tarefa a realizar hoje: ");
+// let tarefa2 = prompt("Informe a 2º tarefa a realizar hoje: ");
+// let tarefa3 = prompt("Informe a 3º tarefa a realizar hoje: ");
+// listaDeTarefas.push(tarefa1);
+// listaDeTarefas.push(tarefa2);
+// listaDeTarefas.push(tarefa3);
+// c)
+// console.log(listaDeTarefas);
+// d)
+// let indiceRemove = Number(
+//   prompt("Informe o Indice da tarefa a ser deletada (0, 1, 2): ")
+// );
+// e)
+// listaDeTarefas.splice(indiceRemove, 1);
+// f)
+// console.log(listaDeTarefas);
+
+// Desafios
+// 1)
+// let frase = prompt("Insira sua frase");
+// let palavras = frase.split(" ");
+// console.log(palavras);
+// 2)
+// const frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+// let indexAbacaxi = frutas.indexOf("Abacaxi");
+// let tamanhoArray = frutas.length;
+// console.log(
+//   `O comprimento da array é ${tamanhoArray} e a palavra Abacaxi esta no Indice ${indexAbacaxi}`
+// );
